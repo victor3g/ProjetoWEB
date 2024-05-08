7102baad33a8a0d6a29523e7498c3bb288095ae0
Victor Gomes Cavalcante <victorgomessc@gmail.com>
2024-05-08 16:12:23 -0300

    implementando novos planetas e ajustes no css e na responsividade

--- apinazaptbr.js
+++ apinazaptbr.js
@@ -30,19 +30,36 @@ const apiKey = 'P9UW2J1p0p6HlOv8K5YrjyduKlTXA81GOatDGwI9';
             }
         }
 
-        // Fazendo uma solicitação GET para a API APOD
-        fetch(apiUrl)
-        .then(response => response.json())
-        .then(async data => {
-            // Exibindo a imagem e informações na página
-            document.getElementById('apod-image').src = data.url;
-            
-            // Traduzindo o título e a explicação
-            const translatedTitle = await translateText(data.title, sourceLanguage, targetLanguage);
-            const translatedExplanation = await translateText(data.explanation, sourceLanguage, targetLanguage);
-            
-            // Exibindo o título e a explicação traduzidos na página
-            document.getElementById('apod-title').textContent = translatedTitle;
-            document.getElementById('apod-explanation').textContent = translatedExplanation;
-        })
-        .catch(error => console.error('Erro ao recuperar os dados:', error));
+    // Fazendo uma solicitação GET para a API APOD
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(async data => {
+    // Limpa qualquer conteúdo anterior da div
+    document.getElementById('media-container').innerHTML = '';
+
+    if (data.media_type === 'image') {
+        // Se for uma imagem, cria e exibe um elemento de imagem
+        const imgElement = document.createElement('img');
+        imgElement.src = data.url;
+        imgElement.alt = 'APOD Image';
+        document.getElementById('media-container').appendChild(imgElement);
+    } else if (data.media_type === 'video') {
+        // Se for um vídeo, cria e exibe um elemento de iframe
+        const iframeElement = document.createElement('iframe');
+        iframeElement.src = data.url;
+        iframeElement.width = '75%';
+        iframeElement.height = '400';
+        iframeElement.frameborder = '0';
+        iframeElement.allowFullscreen = true;
+        document.getElementById('media-container').appendChild(iframeElement);
+    }
+    
+    // Traduzindo o título e a explicação
+    const translatedTitle = await translateText(data.title, sourceLanguage, targetLanguage);
+    const translatedExplanation = await translateText(data.explanation, sourceLanguage, targetLanguage);
+    
+    // Exibindo o título e a explicação traduzidos na página
+    document.getElementById('apod-title').textContent = translatedTitle;
+    document.getElementById('apod-explanation').textContent = translatedExplanation;
+})
+.catch(error => console.error('Erro ao recuperar os dados:', error));
